2400cf2951e988659d1d33b9ce1dc2a7cb0af2d1
felipeZ <tifonzafel@gmail.com>
2017-11-28 14:10:41 +0100

    removed mdp from gromit output

--- examples/run_gromacs.py
+++ examples/run_gromacs.py
@@ -24,7 +24,7 @@ cc.start_managed_service(srv)
 # Create a job and set workflow and inputs
 remove_previous(srv)
 print("Creating job")
-job = srv.create_job('example_job123')
+job = srv.create_job('example_job12')
 job.set_workflow('md_workflow.cwl')
 job.add_input_file('protein_pdb', 'CYP19A1vs.pdb')
 job.add_input_file('protein_top', 'CYP19A1vs.top')
@@ -32,6 +32,7 @@ job.add_input_file('protein_itp', 'CYP19A1vs-posre.itp')
 job.add_input_file('ligand_pdb', 'BHC89.pdb')
 job.add_input_file('ligand_top', 'BHC89.itp')
 job.add_input_file('ligand_itp', 'BHC89-posre.itp')
+job.add_input_file('include_itp', 'attype.itp')
 job.set_input('force_field', 'amber99SB')
 job.set_input('sim_time', 0.001)
 
@@ -53,15 +54,15 @@ cc.stop_managed_service(srv)
 
 # Here, you would quit Python, shut down the computer, etc.
 # To resume where we left off
-srv = cs.service_from_dict(persisted_srv)
-if not cs.managed_service_is_running(srv):
-    cs.start_managed_service(srv)
+srv = cc.service_from_dict(persisted_srv)
+if not cc.managed_service_is_running(srv):
+    cc.start_managed_service(srv)
 
 job = srv.get_job_by_id(persisted_job_id)
 
 # Wait for job to finish
 while job.is_running():
-    sleep(10)
+    sleep(3)
 
 # Process output
 if job.state == 'Success':
