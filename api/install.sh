5015bf87449a55b0575d73f7dc31d5f5c0deef56
felipeZ <tifonzafel@gmail.com>
2017-12-01 14:34:20 +0100

    try to install and run cwltool #9

--- api/install.sh
+++ api/install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 CERISE_API_FILES="$1"
+PYTHON_MODULE='python/2.7.13'
 
 if [ -d "$CERISE_API_FILES/mdstudio/github/cerise-mdstudio-das5" ] ; then
     cd "$CERISE_API_FILES/mdstudio/github/cerise-mdstudio-das5"
@@ -17,5 +18,8 @@ if [ ! -f "$CERISE_API_FILES/mdstudio/bin/getEnergies.py" ] ; then
     SCRIPT="https://raw.githubusercontent.com/MD-Studio/MDStudio/prototype/components/lie_md/lie_md/scripts/getEnergies.py"
     wget $SCRIPT -P "$CERISE_API_FILES/mdstudio/bin"
     chmod u+x "$CERISE_API_FILES/mdstudio/bin/getEnergies.py"
-    pip install panedr --user
 fi
+
+# Install Python dependencies locally
+module load $PYTHON_MODULE
+pip install panedr cwltool --user
