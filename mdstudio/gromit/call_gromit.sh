84ecbfab29088ebe311c55e1e1e24863b617ab10
felipeZ <tifonzafel@gmail.com>
2017-12-12 17:20:58 +0100

    fixed typo in miniconda and change execution permision #11

--- mdstudio/gromit/call_gromit.sh
+++ mdstudio/gromit/call_gromit.sh
@@ -14,7 +14,8 @@ module load cuda75/fft/7.5.18
 module load cuda75/gdk/352.79
 module load cuda75/toolkit/7.5.18
 module load openmpi/gcc/64/1.10.1
-source $CERISE_API_FILES/miniconda/bin/acitvate root
+
+source $CERISE_API_FILES/miniconda/bin/activate root
 
 GMXRC_FILE="$CERISE_API_FILES/mdstudio/github/cerise-mdstudio-das5/mdstudio/gromacs/gromacs-2016.3/bin/GMXRC.bash"
 GROMIT="$CERISE_API_FILES/mdstudio/github/cerise-mdstudio-das5/mdstudio/gromit/gromit_mpi.sh"
