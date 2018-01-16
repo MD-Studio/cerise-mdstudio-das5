dcdeaae481ef3219192564a25d6660f05de123e9
felipeZ <tifonzafel@gmail.com>
2018-01-16 15:27:30 +0100

    used arrays instead of strings in CWL

--- mdstudio/energies/call_decompose.sh
+++ mdstudio/energies/call_decompose.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 CERISE_API_FILES="$1"
-RESIDUES="$2"
-GRO_FILE="$3"
-NDX_FILE="$4"
-TRR_FILE="$5"
-TOP_FILE="$6"
-MDP_FILE="$7"
 
+# remove the CERISE_API_FILES from the input array
+shift
+
+# Activate environment
 source $CERISE_API_FILES/miniconda/bin/activate root
 GMXRC_FILE="$CERISE_API_FILES/mdstudio/github/cerise-mdstudio-das5/mdstudio/gromacs/gromacs-2016.3/bin/GMXRC.bash"
-$CERISE_API_FILES/miniconda/bin/python2.7 $CERISE_API_FILES/mdstudio/bin/getEnergies.py decompose -gmxrc $GMXRC_FILE -res $RESIDUES -gro $GRO_FILE -ndx $NDX_FILE -trr $TRR_FILE -top $TOP_FILE -mdp $MDP_FILE -o decompose.ene
+
+# Call script
+$CERISE_API_FILES/miniconda/bin/python2.7 $CERISE_API_FILES/mdstudio/bin/getEnergies.py decompose -gmxrc $GMXRC_FILE -o decompose.ene $*
--- mdstudio/energies/call_getenergies.sh
+++ mdstudio/energies/call_getenergies.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 CERISE_API_FILES="$1"
-PATH_EDR="$2"
+
+# remove the CERISE_API_FILES from the input array
+shift
 
 source $CERISE_API_FILES/miniconda/bin/activate root
-$CERISE_API_FILES/miniconda/bin/python2.7 $CERISE_API_FILES/mdstudio/bin/getEnergies.py energy -edr $PATH_EDR -o energy.ene
+
+# Run script
+$CERISE_API_FILES/miniconda/bin/python2.7 $CERISE_API_FILES/mdstudio/bin/getEnergies.py energy -o energy.ene $*
