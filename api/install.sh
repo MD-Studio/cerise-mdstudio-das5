20f5a5e84bee2629829876a3e4ddd143a9cbd6a2
felipe zapata <tifonzafel@gmail.com>
2018-09-28 13:20:41 +0200

    used v0.2.1 of cerise-mdstudio-share-data

--- api/install.sh
+++ api/install.sh
@@ -7,7 +7,7 @@ INSTALL_SCRIPT="$CERISE_API_FILES/mdstudio/bin/install_share.sh"
 
 if [ ! -f $INSTALL_SCRIPT ] ; then
     # Download installation
-    SCRIPT="https://raw.githubusercontent.com/MD-Studio/cerise-mdstudio-share-data/v0.2.0/scripts/install_share.sh"
+    SCRIPT="https://raw.githubusercontent.com/MD-Studio/cerise-mdstudio-share-data/v0.2.1/scripts/install_share.sh"
     wget $SCRIPT -P "$CERISE_API_FILES/mdstudio/bin"
     chmod u+x "$CERISE_API_FILES/mdstudio/bin/install_share.sh"
 fi
